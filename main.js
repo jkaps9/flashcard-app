2b0bdae46a00e681f93ada22ef7ad6ffeb9ebfd1
jkaps9 <jkaps9@users.noreply.github.com>
2025-12-07 19:45:16 +0000

    deploy: 2e5e890bd7b9fcc0afe0c4baf74a0acf053139ac

--- main.js
+++ main.js
@@ -35,3 +35,31 @@ function createParagraph(text) {
   para.innerHTML = text;
   return para;
 }
+
+// Mode Toggle
+const modeToggleButtons = document.querySelectorAll(".mode-toggle>button");
+
+const allCardsSection = document.querySelector("section.all-cards");
+const studySection = document.querySelector("section.study");
+
+if (modeToggleButtons) {
+  modeToggleButtons.forEach((button) =>
+    button.addEventListener("click", () => {
+      removeClass("active", modeToggleButtons);
+      button.classList.add("active");
+      if (studySection && allCardsSection) {
+        if (button.id === "study-mode") {
+          allCardsSection.classList.add("hidden");
+          studySection.classList.remove("hidden");
+        } else if (button.id === "all-cards") {
+          studySection.classList.add("hidden");
+          allCardsSection.classList.remove("hidden");
+        }
+      }
+    }),
+  );
+}
+
+function removeClass(className, elements) {
+  elements.forEach((element) => element.classList.remove(className));
+}

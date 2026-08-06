5d0d9e9acfc12368f915c54968e95d39727e4a97
Josh <15219491+jkaps9@users.noreply.github.com>
2026-08-06 00:39:46 -0400

    open close menu on click

--- src/components/DropdownList.jsx
+++ src/components/DropdownList.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import styles from "./DropdownList.module.css";
 
 export default function DropdownList({
@@ -5,33 +6,38 @@ export default function DropdownList({
   handleChange,
   selectedItems,
 }) {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <>
       <button
         type="button"
         className="btn"
-        aria-expanded="false"
+        aria-expanded={isOpen}
         id="categoryFilterButton"
+        onClick={() => setIsOpen(!isOpen)}
       >
         All Categories
       </button>
-      <fieldset className={styles.dropdownList}>
-        {listItems.map((category) => (
-          <div key={category.name}>
-            <input
-              type="checkbox"
-              id={category.name}
-              name="category"
-              value={category.name}
-              checked={selectedItems.includes(category.name)}
-              onChange={handleChange}
-            />
-            <label htmlFor={category.name}>
-              {category.name} ({category.count})
-            </label>
-          </div>
-        ))}
-      </fieldset>
+      {isOpen && (
+        <fieldset className={styles.dropdownList}>
+          {listItems.map((category) => (
+            <div key={category.name}>
+              <input
+                type="checkbox"
+                id={category.name}
+                name="category"
+                value={category.name}
+                checked={selectedItems.includes(category.name)}
+                onChange={handleChange}
+              />
+              <label htmlFor={category.name}>
+                {category.name} ({category.count})
+              </label>
+            </div>
+          ))}
+        </fieldset>
+      )}
     </>
   );
 }

07be5fd16166267ba8362591dff746b7dd9bce50
Josh <15219491+jkaps9@users.noreply.github.com>
2026-08-05 23:40:46 -0400

    modify footer buttons

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -31,11 +31,36 @@ export default function Card({
           <p className="progress__amount">{knownCount}/5</p>
         </div>
         <div className="footer__buttons">
-          <button className="flashcard__menu-btn">
+          <button
+            id="menu-trigger"
+            className="flashcard__menu-btn"
+            aria-haspopup="true"
+            aria-expanded="false"
+            aria-controls="menu-popup"
+            aria-label="Actions"
+          >
             <img src={MenuIcon} aria-hidden="true" />
             <span className="sr-only">Toggle Menu</span>
           </button>
-          {/* <button onClick={() => onDelete(id)}>Delete</button> */}
+
+          <ul id="menu-popup" role="menu" aria-labelledby="menu-trigger" hidden>
+            <li role="none">
+              <button role="menuitem" tabindex="-1" id="action-edit">
+                Edit
+              </button>
+            </li>
+            <li role="none">
+              <button
+                role="menuitem"
+                tabindex="-1"
+                id="action-delete"
+                className="delete-btn"
+                onClick={() => onDelete(id)}
+              >
+                Delete
+              </button>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
